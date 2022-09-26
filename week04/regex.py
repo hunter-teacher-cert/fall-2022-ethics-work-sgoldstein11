baaa38e2000aad5c58a3fb02cbf31df180a3d579
sgoldstein11 <stacygoldstein@gmail.com>
2022-09-25 23:04:27 -0400

    Update regex.py

--- week04/regex.py
+++ week04/regex.py
@@ -1,1 +1,29 @@
 
+import re
+
+
+def find_name(line):
+
+    
+    pattern = r'Mrs.\s[A-Z]\.\s[A-Z]\w*'
+    result = re.findall(pattern,line)
+    pattern =  r'Mrs.\s[A-Z]\w*\s[A-Z]\w*'
+    result = result +re.findall(pattern,line)
+    pattern = r'Mr.\s[A-Z]\w*'
+    result = result +re.findall(pattern,line)
+    pattern = r'[A-Z]\w*\s[A-Z]\w*'
+    result = result +re.findall(pattern,line)
+
+
+    return result
+
+
+f = open("names.txt")
+for line in f.readlines():
+    #print(line)
+    result = find_name(line)
+    if (len(result)>0):
+        print(result)
+
+   
+  
